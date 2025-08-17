0359ac25ce582d31ca4e61464b7eb13562b257cb
dafian12 <akbardavian338@gmail.com>
2025-08-17 23:16:54 +0700

    Create _middleware.js

---
+++ api/_middleware.js
@@ -0,0 +1,9 @@
+export default function middleware(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+}
